c163edb353540b40e61a432cbb0c62668efd547d
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2019-08-24 15:48:52 +0530

    First commit

---
+++ findmax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24 15:39:31 2019
+
+@author: devyanshitiwari
+"""
+def maxno(l):
+    g=0
+    for i in l:
+        if g<i:
+            g=i
+        
+    return g    
